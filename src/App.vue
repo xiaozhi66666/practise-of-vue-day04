d81ee79d502acf0b985cbdca51eae0417d3cdc81
zz <1871490331@qq.com>
2022-07-01 13:23:56 +0800

    完成案例二

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
   <div>
-    <p>请选择你要购买的书籍</p>
-    <ul>
-      <li v-for="(item, index) in arr" :key="index">
-        <button @click="addFn(index)">{{ item.name }}</button>
-      </li>
-    </ul>
-    <table border="1" width="500" cellspacing="0">
-      <tr>
-        <th>序号</th>
-        <th>书名</th>
-        <th>单价</th>
-        <th>数量</th>
-        <th>合计</th>
-      </tr>
-      <tr v-for="(item, index) in arr" :key="index">
-        <td>{{ index + 1 }}</td>
-        <td>{{ item.name }}</td>
-        <td>{{ item.price }}</td>
-        <td>{{ item.count }}</td>
-        <td>{{ item.count * item.price }}</td>
-      </tr>
-    </table>
-    <p>总价格为:{{ totalPrice }}</p>
+    <!-- 无id时, 可以使用index(反正也是就地更新) -->
+    <div
+      v-for="(item, index) in arr"
+      style="display: inline-block"
+      :key="index"
+    >
+      <input type="checkbox" v-model="checkNumArr" :value="item" />
+      <span>{{ item }}</span>
+    </div>
+    <p>你选中的元素, 累加的值和为: {{ theSum }}</p>
   </div>
 </template>
 
@@ -30,43 +17,19 @@
 export default {
   data() {
     return {
-      arr: [
-        {
-          name: "水浒传",
-          price: 107,
-          count: 0,
-        },
-        {
-          name: "西游记",
-          price: 192,
-          count: 0,
-        },
-        {
-          name: "三国演义",
-          price: 219,
-          count: 0,
-        },
-        {
-          name: "红楼梦",
-          price: 178,
-          count: 0,
-        },
-      ],
+      arr: [9, 15, 19, 25, 29, 31, 48, 57, 62, 79, 87],
+      checkNumArr: [], //  保存用户选中的数字
     };
   },
-  methods: {
-    addFn(ind) {
-      this.arr[ind].count++;
-    },
-  },
-  // 定义计算属性
   computed: {
-    totalPrice() {
-      return this.arr.reduce(
-        (sum, item) => (sum += item.count * item.price),
-        0
-      );
+    theSum() {
+      return this.checkNumArr.reduce((sum, val) => {
+        return (sum += val);
+      }, 0);
     },
   },
 };
 </script>
+
+<style>
+</style>
