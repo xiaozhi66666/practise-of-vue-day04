9e637d0140542f4e95a8f1f115b01071f525b4f4
zz <1871490331@qq.com>
2022-07-01 23:26:31 +0800

    完成了三个问题总结

---
+++ src/components/完成作业-购物车案例.vue
@@ -0,0 +1,86 @@
+<template>
+  <div id="app">
+    <table class="tb">
+      <tr>
+        <th><input type="checkbox" />全选</th>
+        <th>商品</th>
+        <th>单价</th>
+        <th>数量</th>
+        <th>小记</th>
+        <th>操作</th>
+      </tr>
+      <!-- 循环渲染的元素tr -->
+      <tr>
+        <td><input type="checkbox" /></td>
+        <td>商品</td>
+        <td>单价</td>
+        <td><span>-</span><input type="text" /><span>+</span></td>
+        <td>小记</td>
+        <td><button>删除</button></td>
+      </tr>
+
+      <tr v-if="list.length === 0">
+        <td colspan="4">没有数据咯~</td>
+      </tr>
+    </table>
+    <br />
+    <button>删除选中商品</button>
+    <button>清理购物车</button>
+    <br />
+    <div style="margin-top: 20px">
+      <h2>统计</h2>
+      <p>已经选中商品件数</p>
+      <p>总价</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        { id: 1, name: "奔驰", time: "2020-08-01" },
+        { id: 2, name: "宝马", time: "2020-08-02" },
+        { id: 3, name: "奥迪", time: "2020-08-03" },
+      ],
+    };
+  },
+  methods: {
+    del(index) {
+      // 删除按钮 - 得到索引, 删除数组里元素
+      this.list.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  width: 600px;
+  margin: 10px auto;
+}
+
+.tb {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+.tb th {
+  background-color: #0094ff;
+  color: white;
+}
+
+.tb td,
+.tb th {
+  padding: 5px;
+  border: 1px solid black;
+  text-align: center;
+}
+
+.add {
+  padding: 5px;
+  border: 1px solid black;
+  margin-bottom: 10px;
+}
+</style>
