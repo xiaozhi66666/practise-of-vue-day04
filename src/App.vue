b3cec211a9ac5ca9f512046d174159d2dca97400
zz <1871490331@qq.com>
2022-07-01 14:21:46 +0800

    完成过滤器作业2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div>
-    <div>国内网站：粉丝数量{{ count | rounding("千") }}</div>
-    <div>国外网站：粉丝数量{{ count | rounding("万") }}</div>
+    <div>时间：{{ time | timeInit("-") }}</div>
+    <div>时间：{{ time | timeInit("/") }}</div>
   </div>
 </template>
 
 <script>
+import moment from "moment";
 export default {
   name: "Demo04App",
-  // 定义一个局部过滤器
-  filters: {
-    rounding(val, unit) {
-      if (unit === "千") {
-        return val / 1000 + unit;
-      } else {
-        return val / 10000 + unit;
-      }
-    },
-  },
 
   data() {
     return {
-      count: 1876986,
+      time: +new Date(),
     };
+  },
+  filters: {
+    timeInit(val, s) {
+      return moment(val).format("YYYY" + s + "MM" + s + "DD");
+    },
   },
 
   mounted() {},
